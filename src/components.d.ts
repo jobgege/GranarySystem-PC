f18f3160ae68a95eb3719ecb5cf3502a916b28b6
agaric303 <agaric_303@163.com>
2024-03-02 22:46:14 +0800

    登录页面响应式、引入滑块登录

--- src/components.d.ts
+++ src/components.d.ts
@@ -10,6 +10,7 @@ declare module 'vue' {
     ElAside: typeof import('element-plus/es')['ElAside']
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElCard: typeof import('element-plus/es')['ElCard']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDropdown: typeof import('element-plus/es')['ElDropdown']

92ff2b1cb310933d9059508ee562f60de519c53e
agaric303 <agaric_303@163.com>
2024-03-08 16:52:22 +0800

    入门登记新增头部操作及表格样式

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {createPinia} from "pinia";
 import piniaPluginPersist from 'pinia-plugin-persist'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import echarts from 'echarts'; 
+import * as echarts from 'echarts';
 import './index.css'
 
 
